3869dbe1bb7ab3d3ab96631650e21affdb7a5e26
Radouane TAMOUSS <atamousse.red@gmail.com>
2024-09-27 17:36:15 +0100

    check the file, check extension to be (.cub) and check if file exists or not, and init the strucutres i will use

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 20:55:26 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/09/27 14:50:03 by rtamouss         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rtamouss <rtamouss@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/09/17 20:55:26 by eouhrich      #+#    #+#                 */
+/*   Updated: 2024/09/27 17:29:45 by rtamouss      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,49 @@
 // 	get_data()->ceiling_color = CYAN;// TODO this just for startin befroe parsing is complete
 // }
 
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
 
+// check file 
+	// check extension
+	// 
+
+int check_file(char *str, int *fd)
+{
+	int i = 0;
+	char *res;
+	ssize_t bytes_read;
+	char buff[5];
+	if (str == NULL)
+		return (0);
+	res = ft_strrchr(str, '.');
+	if (res == NULL || ft_strcmp(res, ".cub") != 0)
+		return (printf("invalid file\n"), 0);
+	*fd = open(str, O_RDONLY);
+	if (*fd < 0)
+		return (printf("Open failed\n"), 0);
+	bytes_read = read(*fd, &buff, 1);
+	if (bytes_read == 0)
+		return (printf("File is empty\n"), 0);
+	else if (bytes_read < 0)
+		return (printf("Read failed\n"), 0);
+	
+	return (1);
+}
 
 int	main(int ac, char **av)
 {
+	int		fd;
+
+	if (ac != 2 || check_file(av[1], &fd) == 0)
+		return (printf("Error\nUsage: ./Cube3d map.cub\n"), 0);
 	(void) ac;
 	(void) av;
 
