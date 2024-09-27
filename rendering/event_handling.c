d14ed7aa320d445603761e6a5d3644eabd9c86d4
ErabaReta <mokhtarouhrich@gmail.com>
2024-09-27 18:28:42 +0100

    updating the data struct

--- rendering/event_handling.c
+++ rendering/event_handling.c
@@ -9,10 +9,20 @@ int	ft_close(void)
 int	handle_keys(int keycode, void *garbage)
 {
 	(void)garbage;
+	printf("keycode => %d\n", keycode);
 	if (keycode == ESC)
 	{
 		mlx_destroy_window(get_data()->mlx, get_data()->win);
 		exiter(0);
 	}
+	else if (keycode == W)
+		get_data()->player_y -= 10;
+	else if (keycode == S)
+		get_data()->player_y += 10;
+	else if (keycode == D)
+		get_data()->player_x += 10;
+	else if (keycode == A)
+		get_data()->player_x -= 10;
+	printf("===== coords (%d, %d)\n", get_data()->player_x, get_data()->player_y);
 	return (0);
 }
