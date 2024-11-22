b3aec90cc880c384d717484983fc979bb78e3ada
ErabaReta <mokhtarouhrich@gmail.com>
2024-11-22 02:42:26 +0100

    remove the new added leaks + make a safer version of function that could fail

--- garbage_collector/heap_controller.h
+++ garbage_collector/heap_controller.h
@@ -6,7 +6,7 @@
 /*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 01:17:41 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/11/21 01:20:12 by eouhrich         ###   ########.fr       */
+/*   Updated: 2024/11/22 02:28:37 by eouhrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,10 @@ void	*mallocate(size_t size);
 void	free_all_heap(void);
 void	free_ptr(void *ptr);
 
+// a safer version of the functions that could possibly fail
+void	*safer_xpm_file_to_image(void *xvar,char *filename,
+			       int *width,int *height);
+void    *safer_get_data_addr(void *img, int *bits_per_pixel,
+			int *size_line, int *endian);
+				   
 #endif
